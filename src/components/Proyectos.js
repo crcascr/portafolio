a888e98fb0416509f971519765f4eac3e787197d
crcascr <cristianj_20@hotmail.com>
2024-01-03 13:19:32 -0500

    Adicion proyecto booking

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -27,8 +27,50 @@ function Proyectos(props) {
         },
       ],
       color: "#dcbbe966",
+      hoverColor: "#dcbbe9",
       descripcion:
-      "Un sitio web elegante y funcional con un sólido backend desarrollado en PHP y MySQL que facilita la gestión de servicios, citas, clientes, administradores, ubicaciones y profesionales. Su interfaz dinámica y responsive, construida con JavaScript y CSS, ofrece características como un mapa interactivo, formulario de contacto, sistema de reservas en línea y un práctico panel de administración. Alojado en un servidor seguro y optimizado para motores de búsqueda, es la plataforma perfecta para servicios de belleza y relajación."
+        "Un sitio web elegante y funcional con un sólido backend desarrollado en PHP y MySQL que facilita la gestión de servicios, citas, clientes, administradores, ubicaciones y profesionales. Su interfaz dinámica y responsive, construida con JavaScript y CSS, ofrece características como un mapa interactivo, formulario de contacto, sistema de reservas en línea y un práctico panel de administración. Alojado en un servidor seguro y optimizado para motores de búsqueda, es la plataforma perfecta para servicios de belleza y relajación.",
+    },
+    {
+      carpeta: "booking",
+      titulo: "Booking App",
+      link: "https://github.com/crcascr/hotel-booking-app",
+      imagenes: [
+        {
+          nombre: "booking1",
+          src: require("../images/proyectos/booking/booking1.webp"),
+        },
+        {
+          nombre: "booking2",
+          src: require("../images/proyectos/booking/booking2.webp"),
+        },
+        {
+          nombre: "booking3",
+          src: require("../images/proyectos/booking/booking3.webp"),
+        },
+        {
+          nombre: "booking4",
+          src: require("../images/proyectos/booking/booking4.webp"),
+        },
+        {
+          nombre: "booking5",
+          src: require("../images/proyectos/booking/booking5.webp"),
+        },
+      ],
+      color: "#acc7c766",
+      hoverColor: "#acc7c7",
+      descripcion:
+        "Una aplicación móvil funcional desarrollada con React, Bootstrap y CSS que facilita la reserva de habitaciones de hotel. Cuenta con una interfaz ágil y elegante optimizada para iOS y Android que muestra información clave de los hoteles y permite filtrar, buscar y ordenar de forma sencilla. La aplicación utiliza un modal elegante para mostrar más información de cada hotel. Construida con metodologías ágiles y tecnologías web progresivas.",
+      extraLinks: [
+        {
+          nombre: "Android",
+          link: "https://appetize.io/app/6e7detlk4btpdo2erfgfbwgibu?device=pixel7&osVersion=13.0",
+        },
+        {
+          nombre: "iOS",
+          link: "https://appetize.io/app/qhz6wvnql3gaqxsgcnocfc2wsu?device=iphone15promax&osVersion=17.0",
+        },
+      ],
     },
     {
       carpeta: "sumz",
@@ -53,6 +95,7 @@ function Proyectos(props) {
         },
       ],
       color: "#f59e0b66",
+      hoverColor: "#f59e0b",
       descripcion:
         "Una práctica herramienta que te permite obtener resúmenes en español de tus artículos favoritos en línea, con solo unos pocos clics. Utiliza la potente API de RapidAPI Article Extractor and Summarizer para ofrecer un servicio de resumen de alta calidad.",
     },
@@ -79,6 +122,7 @@ function Proyectos(props) {
         },
       ],
       color: "#4a4e7466",
+      hoverColor: "#4a4e74",
       descripcion:
         "Una aplicación web que permite almacenar y organizar notas en una base de datos de Google Firebase. Las notas se ordenan automáticamente según la fecha de modificación.",
     },
@@ -105,6 +149,7 @@ function Proyectos(props) {
         },
       ],
       color: "#5035ff66",
+      hoverColor: "#5035ff",
       descripcion:
         "Un emocionante juego en el que el objetivo es igualar las caras de los 10 dados en el menor tiempo posible. El juego registra el tiempo de juego, el número de movimientos y el mejor tiempo, este último en el almacenamiento local. Si logras una nueva marca, se actualizará automáticamente.",
     },
@@ -131,6 +176,7 @@ function Proyectos(props) {
         },
       ],
       color: "#4d5b9e66",
+      hoverColor: "#4d5b9e",
       descripcion:
         "Un emocionante juego de preguntas que pondrá a prueba tus conocimientos y tu agudeza mental. Quizzical obtiene preguntas de diversas categorías y niveles de dificultad a través de la API de Open Trivia Database. Estas preguntas, junto con sus opciones de respuesta, se presentan de manera aleatoria en un desafío interactivo.",
     },
@@ -147,16 +193,17 @@ function Proyectos(props) {
         </h3>
         <div
           className="proyecto--container"
-          style={{ backgroundColor: elemento.color }}
+          style={{
+            backgroundColor: elemento.color,
+            boxShadow:
+              tarjetaProyectoHov === index
+                ? `0 0 40px 10px ${elemento.hoverColor}`
+                : "",
+          }}
           onMouseEnter={() => setTarjetaProyectoHov(index)}
           onMouseLeave={() => setTarjetaProyectoHov(null)}
         >
-          <div
-            className="proyecto--descripcion"
-            style={{
-              boxShadow: tarjetaProyectoHov === index ? `0 0 20px ${props.modoOscuro?"white":"black"}` : "",
-            }}
-          >
+          <div className="proyecto--descripcion">
             <p
               className={`proyecto--descripcion-texto ${
                 props.modoOscuro ? "texto--modoOscuro" : ""
@@ -179,6 +226,28 @@ function Proyectos(props) {
               />
               Ver proyecto
             </a>
+            {elemento.extraLinks && (
+              <>
+                {elemento.extraLinks.map((linkExtra, indexLink) => (
+                  <a
+                    key={indexLink}
+                    className={`proyecto--link ${
+                      props.modoOscuro ? "texto--modoOscuro" : ""
+                    }`}
+                    href={linkExtra.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      alt="Icono enlace"
+                      className="proyecto--link-icono"
+                      src={props.modoOscuro ? linkBlanco : link}
+                    />
+                    {`Ver proyecto en ${linkExtra.nombre}`}
+                  </a>
+                ))}
+              </>
+            )}
           </div>
           <div className={`proyecto--imagenes`}>
             {elemento.imagenes.map((imagen) => (
@@ -210,7 +279,9 @@ function Proyectos(props) {
   return (
     <section id="proyectos" className="proyectos">
       <h2 className="proyectos--titulo">
-        <span className="texto--color">Proyectos: Código que Cuenta Historias</span>
+        <span className="texto--color">
+          Proyectos: Código que Cuenta Historias
+        </span>
       </h2>
       <p
         className={`proyectos--introduccion ${
